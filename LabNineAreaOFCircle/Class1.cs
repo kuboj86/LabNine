ef60acb686880dbe6efa715f91651f2a17bf6d70
Jason Kubo <kuboj86@gmail.com>
2017-11-03 08:41:23 -0400

    Add project files.

---
+++ LabNineAreaOFCircle/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNineAreaOFCircle
+{
+    class Circle
+    {
+        private double _radius;
+
+        public double radius
+        {
+            get { return this._radius; }
+            set { this._radius = radius; }
+        }
+
+        public Circle(double radius)
+        {
+            this._radius = radius;
+        }
+        public double GetCircumference()
+        {
+             return 2 * Math.PI * _radius;
+        }
+        public string GetFormattedCircumference()
+        {   
+            return this.FormatNumber(this.GetCircumference());
+        } 
+        public double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+        public string GetFormattedArea()
+        {
+            return this.FormatNumber(this.GetArea());
+        }
+        private string FormatNumber(double x)
+        { 
+            return x.ToString("0.00");
+        }
+    }
+}
---
+++ LabNineAreaOFCircle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNineAreaOFCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool shouldContinue = false;
+            do
+            {
+                Console.Write("Please enter in a number to determine the radius of a circle?:\t");
+                double radius = Convert.ToDouble(Console.ReadLine());
+
+                Circle myCircle = new Circle(radius);
+
+                Console.WriteLine($"The area of the circle is:\t{myCircle.GetFormattedArea()}");
+                Console.WriteLine($"The circumferance of the circle is:\t{myCircle.GetFormattedCircumference()}");
+                shouldContinue = Option();
+            } while (shouldContinue);
+            int count = 0;
+            Console.WriteLine($"ok, Goodbye.");
+        }
+        static bool Option()
+        {
+            {
+                string answer;
+                do
+                {
+                    Console.Write("Would you like to make another circle?:   Y/N ");
+                    answer = Console.ReadLine();
+                } while (!(answer.ToLower() == "y" || answer.ToLower() == "n"));
+                return (answer == "y");
+            }
+        }
+    }
+}
